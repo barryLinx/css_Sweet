4480561f8497e7a54b3f03acccf7c61b65a44b13
Bluce <a1019521a@gmail.com>
2021-10-16 22:11:14 +0800

    first commit manifest.json

---
+++ README.md
@@ -0,0 +1,5 @@
+# 甜點網頁切版
+
+## Design layout
+
+[甜點店商](https://xd.adobe.com/spec/934efdb7-a7e4-47d5-572e-efece0914f62-e57f/)
---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const menuBar = document.querySelector('#menu-bar');
+const navbar = document.querySelector('.navbar');
+
+menuBar.addEventListener('click',(evt)=>{
+  menuBar.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  cartWrap.classList.remove('active');
+})
+// scroll 
+window.onscroll= ()=>{
+  menuBar.classList.remove('fa-times');
+  navbar.classList.remove("active");
+  cartWrap.classList.remove('active');
+  if(window.scrollY > 160){
+    document.querySelector('#scroll-top').classList.add('active')
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active')
+  }
+}
+
+const cartWrap = document.querySelector(".cart-wrap");
+const cartBtn = document.querySelector("#cart-btn");
+
+cartBtn.addEventListener('click',(evt)=>{
+  evt.preventDefault();
+  cartWrap.classList.toggle('active');
+  menuBar.classList.remove('fa-times');
+  navbar.classList.remove('active');
+})
